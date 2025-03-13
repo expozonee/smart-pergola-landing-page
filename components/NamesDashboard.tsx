bc8d343a9710f4269ccbb6fac545c6f63176775b
Ibrahem <boss.abrahem@hotmail.com>
2025-03-13 11:22:40 +0200

    removed fake data object

--- components/NamesDashboard.tsx
+++ components/NamesDashboard.tsx
@@ -1,20 +1,7 @@
-import { User } from "@/types/User";
-
 import { NamesTable } from "./NamesTable/NamesTable";
 import { columns } from "./NamesTable/columns";
 import { use } from "react";
 import { customers } from "@/db/customers";
-
-const date = new Date();
-
-// const FAKE_DATA: User[] = [
-//   {
-//     fullName: "Bob",
-//     phoneNumber: "0555555555",
-//     date: `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`,
-//     city: "עראבה",
-//   },
-// ];
 
 export function NamesDashboard() {
   const { customers: customersData, years } = use(customers());
