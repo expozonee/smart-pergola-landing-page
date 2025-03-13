1b67dce61d84778c871f586466143b7c9b0c6d28
Ibrahem <boss.abrahem@hotmail.com>
2025-03-13 11:35:24 +0200

    replaced the two seperated SuccessAlert and ErrorAlert with one alert component AlertMessage

--- hooks/useAlert.tsx
+++ hooks/useAlert.tsx
@@ -1,19 +1,35 @@
-import { SuccessAlert } from "@/components/SuccessAlert/SucessAlert";
+import { AlertMessage } from "@/components/Alerts/Alert";
+// import { ErrorAlert } from "@/components/Alerts/ErrorAlert";
+// import { SuccessAlert } from "@/components/Alerts/SucessAlert";
 import { Dispatch, JSX, SetStateAction, useEffect, useState } from "react";
 
+export type AlertState = {
+  show: boolean;
+  type: "success" | "error";
+  message?: unknown | string | undefined;
+};
+
 export function useAlert() {
-  const [showAlert, setShowAlert] = useState(false);
+  const [showAlert, setShowAlert] = useState<AlertState>({
+    show: false,
+    type: "success",
+  });
 
   useEffect(() => {
     const timeout = setTimeout(() => {
-      setShowAlert(false);
+      setShowAlert((prev) => {
+        return {
+          ...prev,
+          show: false,
+        };
+      });
     }, 3000);
     return () => clearTimeout(timeout);
   }, [showAlert]);
 
-  return [showAlert, setShowAlert, SuccessAlert] as [
-    boolean,
-    Dispatch<SetStateAction<boolean>>,
-    () => JSX.Element
+  return [showAlert, setShowAlert, AlertMessage] as [
+    AlertState,
+    Dispatch<SetStateAction<AlertState>>,
+    ({ type, message }: Omit<AlertState, "show">) => JSX.Element
   ];
 }
