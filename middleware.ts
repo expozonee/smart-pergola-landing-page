594f3bb23a96621ca888bc86dc7e4fd6e4386bee
Ibrahem <boss.abrahem@hotmail.com>
2025-04-29 10:11:57 +0300

    removed commented code

--- middleware.ts
+++ middleware.ts
@@ -1,16 +1,16 @@
 import { NextResponse } from "next/server";
-import { NextRequest } from "next/server";
+import { auth } from "./auth";
 
-export function middleware(request: NextRequest) {
-  const isAuth = request.nextUrl.searchParams.get("ok");
+export default auth((req) => {
+  const isLoggedIn = !!req.auth;
 
-  if (isAuth === "true") {
+  if (isLoggedIn) {
     return NextResponse.next();
   } else {
-    return NextResponse.redirect(new URL("/", request.url));
+    return NextResponse.redirect(new URL("/login", req.url));
   }
-}
+});
 
 export const config = {
-  matcher: "/admin",
+  matcher: ["/admin/:path*"],
 };
